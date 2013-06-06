8afeb29cea45c642c8cbf63291c053ffb8e43ced
Julien Fontanet <julien.fontanet@isonoe.net>
2013-06-06 15:54:38 +0200

    Object oriented implemented and various optimizations.

--- main.js
+++ main.js
@@ -1,69 +1,82 @@
 #!/usr/bin/nodejs
 
-
-var http = require('https'); //Variable de type HTTP
+var http = require('https'); // Variable de type HTTP.
 var xmlrpc = require('xmlrpc');
 
 ///////////////////////////////////////
 
-function xmlrpc_call(method, params, callback)
+function xapi_call(host, method, params, callback)
 {
-	var option = {
-		hostname: 'andromeda.vates.fr',
+	var options = {
+		hostname: host,
 		port: '443',
 		rejectUnauthorized : false,
 	};
 
-	var req = xmlrpc.createSecureClient(option).methodCall(method, params, function(error, value) {
-		callback(error, value);
+	xmlrpc.createSecureClient(options).methodCall(method, params, function (error, value) {
+		if (error)
+		{
+			console.error(error);
+			process.exit(1);
+		}
+
+		if ('Success' !== value.Status)
+		{
+			console.error(value);
+			process.exit(1);
+		}
+
+		callback(value.Value);
 	});
 }
 
 ///////////////////////////////////////
 
-function connect(login, pass)
+function Xapi(host, session)
 {
-	xmlrpc_call('session.login_with_password', [login, pass], function(error, value){
-		if (!error)
-		{
-			listen(value.Value);
-		}
-		else
-		{
-			console.log('Can\'t connect to XenServer', error);
-		}
-	 });
+	this.host = host;
+	this.session = session;
 }
 
-function listen(value)
+Xapi.prototype.call_ = function(method, callback)
 {
-	xmlrpc_call('event.register', [value, ['*']], function() {
-		xmlrpc_call('event.next', [value], function(error, resp){
-			console.log(resp);
+	var params = [];
+
+	var n = arguments.length;
+	if (n > 2)
+	{
+		params = Array.prototype.slice.call(arguments, 1, n - 1);
+		callback = arguments[n - 1];
+	}
+
+	params.unshift(this.session);
+
+	xapi_call(this.host, method, params, function(value) {
+		callback(value);
+	});
+}
+
+// Static method = class method.
+Xapi.open = function(host, username, password, callback) {
+	xapi_call(host, 'session.login_with_password', [username, password], function(value) {
+		callback(new Xapi(host, value));
+	});
+};
+
+///////////////////////////////////////
+
+//
+Xapi.open('andromeda.vates.fr', 'root', 'dev2013stage', function(xapi) {
+
+	var wait_and_log_event = function() {
+		//
+		xapi.call_('event.next', function(event) {
+			console.log(event);
+
+			wait_and_log_event();
 		});
-	});
-}
-
-///////////////////////////////////////
-
-var args = process.argv.slice(2);
-
-if (!args.length)
-{
-	console.error('missing argument(s)');
-	return;
-}
-
-// --connect <login> <password>
-for (var i = 0, n = args.length; i < n; ++i)
-{
-	if (args[i] === '--connect') // If the argument is --add-host
-	{
-		console.log('command: ' +args[i]);
-		connect(args[++i],args[++i]);
 	}
-	else // If one of all arguments doesn't exist.
-	{
-		console.log('Unknown command in argurment: ' + args[i]);
-	}
-}
+
+	//
+	xapi.call_('event.register', ['*'], wait_and_log_event);
+});
